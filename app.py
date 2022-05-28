33d163f47675a6a0a70eba15b82de7da2cad34d3
MEHAK AGGARWAL <mehakagg1211@gmail.com>
2022-05-28 11:39:17 +0530

    changes added

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(name=None):
-    return render_template('indx.html',name=name)
+    return render_template('index.html',name=name)
 
 @app.route('/exec')
 def parse(name=None):
     import face_recognition
     print("done")
-    return render_template('indx.html',name=name)
+    return render_template('index.html',name=name)
 
 
 if __name__ == '__main__':
